e0c8d4e35af60935536b1f47a319a823c841f994
David <dbermejosimon@gmail.com>
2018-11-09 16:58:00 +0100

    Creación de Branch y clase principal main

---
+++ src/Bermejo_Simon_David/Main.java
@@ -0,0 +1,16 @@
+package Bermejo_Simon_David;
+
+/**
+ * @author David Bermejo Simon
+ * @since 1.0.0
+ **/
+public class Main {
+
+    /**
+     * Hilo principal "main"
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
