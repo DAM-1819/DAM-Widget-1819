bddba209c1ddfe6634d13a9ef680cb07a5f12827
Misael Harinero <missigamer@gmail.com>
2018-11-09 22:49:11 +0100

    Class Principal && VentanaPrincipal
    Clases que se utilizan para el Testeo De nuestro Widget

--- src/misael_harinero/Principal.java
+++ src/misael_harinero/Principal.java
@@ -1,8 +1,18 @@
 package misael_harinero;
 
+import java.awt.EventQueue;
 
+/**
+ * @author missi
+ * @version 1.0
+ * Hilo principal de Ejecucion 
+ */
 public class Principal {
     public static void main(String[] args) {
-        
+    	 EventQueue.invokeLater(()->{
+             VentanaPrincipal ventanaPrincipal = new VentanaPrincipal();
+             ventanaPrincipal.invocar();
+
+         });
     }
 }
