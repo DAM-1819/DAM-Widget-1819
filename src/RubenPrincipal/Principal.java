ce9892b2d576c61c075b44e8d06a113f81c9bbde
Ruben <rhernandezz01@informatica.iesvalledeljerteplasencia.es>
2018-11-12 19:26:16 +0100

    Progreso de las clases

--- src/RubenPrincipal/Principal.java
+++ src/RubenPrincipal/Principal.java
@@ -1,5 +1,18 @@
 package RubenPrincipal;
 
-public class Principal {
+import java.awt.EventQueue;
 
+public class Principal {
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaPrincipal ventana = new VentanaPrincipal();
+					ventana.iniciar();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 }
--- src/RubenPrincipal/VentanaPrincipal.java
+++ src/RubenPrincipal/VentanaPrincipal.java
@@ -1,21 +1,27 @@
 package RubenPrincipal;
 
 import java.awt.GridLayout;
+import java.io.File;
 
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class VentanaPrincipal {
 
 	JFrame ventana;
 	JPanel panelCorazon;
+	ImageIcon corazon;
+	JLabel imagenLabel;
 
 	public VentanaPrincipal() {
 		ventana = new JFrame();
 		ventana.setLayout(new GridLayout(1, 3));
 		ventana.setBounds(300, 300, 900, 500);
 		ventana.setDefaultCloseOperation(1);
+
 	}
 
 	public void iniciar() {
@@ -26,11 +32,29 @@ public class VentanaPrincipal {
 	}
 
 	private void inciarComponenetes() {
+		for (int i = 0; i < 3; i++) {
+			corazon = new ImageIcon("src/RubenPrincipal/corazon.jpg");
+			imagenLabel = new JLabel(corazon);
+					ventana.add(imagenLabel);
 
-		panelCorazon= new JPanel();
-		ImageIcon imagenCorazon = new ImageIcon(getClass().getResource("corazon.png")); 
-		panelCorazon.add(imagenCorazon);
-		
+		}
+
+		ventana.setVisible(true);
+		ventana.setSize(1000, 500);
+		ventana.setResizable(false);
+
+		// for (int i = 0; i < 3; i++) {
+		// panelCorazon = new JPanel();
+		// //panelCorazon.add(corazon);
+		// }
+		// ImageIcon imagenCorazon = new ImageIcon(
+		// getClass().getResource("/DAM-Widget-1819/src/RubenPrincipal/corazon.jpg"));
+		// imagenLabel.add(panelCorazon);
+		// // ImageIO.read(new File("DAM-Widget-1819/src/RubenPrincipal/corazon.jpg"));
+		// //
+		// // devuelve imagen
+		// // redimensionar la imagen para meter en icono
+
 	}
 
 	private void inicarListeners() {
