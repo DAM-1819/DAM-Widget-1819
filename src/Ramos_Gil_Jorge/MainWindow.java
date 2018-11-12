99395fb16f78873aebbc118e113eea9bc5c11cc9
Jorge Ramos <jorgera1996@gmail.com>
2018-11-12 19:20:09 +0100

    JLabels y listeners añadidos

--- src/Ramos_Gil_Jorge/MainWindow.java
+++ src/Ramos_Gil_Jorge/MainWindow.java
@@ -1,12 +1,19 @@
 package Ramos_Gil_Jorge;
 
+import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class MainWindow {
 
 	private JFrame jframe;
+	private JLabel[][] jlabel = new JLabel[3][3];
+	private int i;
+	private int j;
 	
 	public MainWindow() {
 		jframe = new JFrame("Principal");
@@ -16,7 +23,54 @@ public class MainWindow {
 	
 	public void inicializarComponentes() {
 		
+		GridBagConstraints settings;
 		jframe.setLayout(new GridBagLayout());
+		for (i = 0; i < 3; i++) {
+			for (j = 0; j < 3; j++) {
+				jlabel[i][j] = new JLabel("Añade una nota");
+				settings = new GridBagConstraints();
+				settings.gridx = i;
+				settings.gridy = j;
+				jlabel[i][j].addMouseListener(new MouseListener() {
+					
+					@Override
+					public void mouseReleased(MouseEvent e) {
+						// TODO Auto-generated method stub
+						
+					}
+					
+					@Override
+					public void mousePressed(MouseEvent e) {
+						// TODO Auto-generated method stub
+						
+					}
+					
+					@Override
+					public void mouseExited(MouseEvent e) {
+						// TODO Auto-generated method stub
+						
+					}
+					
+					@Override
+					public void mouseEntered(MouseEvent e) {
+						// TODO Auto-generated method stub
+						
+					}
+					
+					@Override
+					public void mouseClicked(MouseEvent e) {
+						// TODO Auto-generated method stub
+						if (e.getButton() == MouseEvent.BUTTON1) {
+							jlabel[i][j].setVisible(false);
+							System.out.println(i);
+							System.out.println(j);
+							jframe.remove(jlabel[i][j]);
+						}
+					}
+				});
+				jframe.add(jlabel[i][j], settings);
+			}
+		}
 	}
 	
 	public void inicializar() {
