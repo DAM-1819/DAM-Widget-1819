2e02ecb959ac6c31ad10de68372ec69562ec66c8
Unknown <agonzalezd07@informatica.iesvalledeljerteplasencia.es>
2018-11-12 15:40:21 +0100

    Widget finalizado

--- src/Axier_Gonzalez/Login.java
+++ src/Axier_Gonzalez/Login.java
@@ -7,13 +7,9 @@ import java.awt.GridLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
-import javax.swing.JComponent;
-import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -21,6 +17,8 @@ import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class Login {
 	JFrame ventana;
@@ -173,7 +171,6 @@ public class Login {
 
 			}
 		});
-
 	}
 
 	public void inicializar() {
--- src/Axier_Gonzalez/Principal.java
+++ src/Axier_Gonzalez/Principal.java
@@ -5,8 +5,7 @@ import java.awt.EventQueue;
 public class Principal {
 
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			
+		EventQueue.invokeLater(new Runnable() {			
 			@Override
 			public void run() {
 				Login log = new Login();
