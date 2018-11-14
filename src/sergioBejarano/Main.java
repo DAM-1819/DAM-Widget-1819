b0c933da33367d3f3c1b218f83e5f1b600caaf5d
Sergio <sbejaranomartin@gmail.com>
2018-11-14 17:52:54 +0100

    FInalizado 1.0
     Borrado de la clase MyIcon y modificado el tamaño de las imágenes y controlada la entrada de texto.

--- src/sergioBejarano/Main.java
+++ src/sergioBejarano/Main.java
@@ -1,8 +1,5 @@
 package sergioBejarano;
 
-import java.awt.GraphicsEnvironment;
-import java.util.Arrays;
-
 public class Main {
 
 	public static void main(String[] args) {
--- src/sergioBejarano/Zodiaco.java
+++ src/sergioBejarano/Zodiaco.java
@@ -8,6 +8,7 @@ import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -20,7 +21,7 @@ public class Zodiaco extends JPanel{
 	JLabel subtitulo;
 	JTextField entrada;
 	JButton aceptar;
-	MyIcon icono;
+	ImageIcon icono;
 
 	public Zodiaco() {
 		this.setLayout(new GridBagLayout());
@@ -65,119 +66,124 @@ public class Zodiaco extends JPanel{
 	}
 	
 	public void mostrarMensaje() {
-		String resultado = comprobar();
-		int respuesta = JOptionPane.showConfirmDialog(null, resultado, "SIGNO ZODIACAL", JOptionPane.YES_NO_OPTION);
-		//int respuesta = JOptionPane.showConfirmDialog(null, resultado, "SIGNO ZODIACAL", JOptionPane.YES_NO_OPTION, icono);
-		if(respuesta == JOptionPane.NO_OPTION) {
-			System.exit(0);
-		} else if(respuesta == JOptionPane.YES_OPTION) {
-			entrada.setText("");
+		if(entrada.getText().matches("[0-3][0-9]\\/[0-1][0-9]\\/[0-2][0-9][0-9][0-9]")) {
+			String resultado = comprobar();
+			int respuesta = JOptionPane.showConfirmDialog(null, resultado+"\n¿Quieres consultar otra fecha?", "SIGNO ZODIACAL",
+					JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE,icono);
+			if(respuesta == JOptionPane.NO_OPTION) {
+				System.exit(0);
+			} else if(respuesta == JOptionPane.YES_OPTION) {
+				entrada.setText("");
+			}
+		} else {
+			JOptionPane.showMessageDialog(null, "Formato de fecha incorrecto", "Error de formato", JOptionPane.CLOSED_OPTION);
 		}
+		
 	}
 	
 	public String comprobar() {
-		String[] fecha = entrada.getText().split("/");
+			String[] fecha = entrada.getText().split("/");
 
-		switch (Integer.parseInt(fecha[1])) {
-			case 1:
-				if(Integer.parseInt(fecha[0])<20) {
-					icono = new MyIcon("/img/capricornio.jpg");
-					return "Capricornio";
-				} else {
-					icono = new MyIcon("/img/acuario.jpg");
-					return "Acuario";
-				}
-			case 2:
-				if(Integer.parseInt(fecha[0])<19) {
-					icono = new MyIcon("/img/acuario.jpg");
-					return "Acuario";
-				} else {
-					icono = new MyIcon("/img/piscis.jpg");
-					return "Piscis";
-				}
-			case 3:
-				if(Integer.parseInt(fecha[0])<21) {
-					icono = new MyIcon("/img/piscis.jpg");
-					return "Piscis";
-				} else {
-					icono = new MyIcon("/img/aries.jpg");
-					return "Aries";
-				}
-			case 4:
-				if(Integer.parseInt(fecha[0])<20) {
-					icono = new MyIcon("/img/aries.jpg");
-					return "Aries";
-				} else {
-					icono = new MyIcon("/img/taruo.jpg");
-					return "Tauro";
-				}
-			case 5:
-				if(Integer.parseInt(fecha[0])<21) {
-					icono = new MyIcon("/img/taruo.jpg");
-					return "Tauro";
-				} else {
-					icono = new MyIcon("/img/geminis.jpg");
-					return "Géminis";
-				}
-			case 6:
-				if(Integer.parseInt(fecha[0])<21) {
-					icono = new MyIcon("/img/geminis.jpg");
-					return "Géminis";
-				} else {
-					icono = new MyIcon("/img/cancer.jpg");
-					return "Cáncer";
-				}
-			case 7:
-				if(Integer.parseInt(fecha[0])<23) {
-					icono = new MyIcon("/img/cancer.jpg");
-					return "Cáncer";
-				} else {
-					icono = new MyIcon("/img/leo.jpg");
-					return "Leo";
-				}
-			case 8:
-				if(Integer.parseInt(fecha[0])<23) {
-					icono = new MyIcon("/img/leo.jpg");
-					return "Leo";
-				} else {
-					icono = new MyIcon("/img/virgo.jpg");
-					return "Virgo";
-				}
-			case 9:
-				if(Integer.parseInt(fecha[0])<23) {
-					icono = new MyIcon("/img/virgo.jpg");
-					return "Virgo";
-				} else {
-					icono = new MyIcon("/img/libra.jpg");
-					return "Libra";
-				}
-			case 10:
-				if(Integer.parseInt(fecha[0])<23) {
-					icono = new MyIcon("/img/libra.jpg");
-					return "Libra";
-				} else {
-					icono = new MyIcon("/img/escorpio.jpg");
-					return "Escorpio";
-				}
-			case 11:
-				if(Integer.parseInt(fecha[0])<22) {
-					icono = new MyIcon("/img/escorpio.jpg");
-					return "Escorpio";
-				} else {
-					icono = new MyIcon("/img/sagitario.jpg");
-					return "Sagitario";
-				}
-			case 12:
-				if(Integer.parseInt(fecha[0])<22) {
-					icono = new MyIcon("/img/sagitario.jpg");
-					return "Sagitario";
-				} else {
-					icono = new MyIcon("/img/capricornio.jpg");
-					return "Capricornio";
-				}
-				default:
-					return "Fecha inválida";
-		}
+			switch (Integer.parseInt(fecha[1])) {
+				case 1:
+					if(Integer.parseInt(fecha[0])<20) {
+						icono = new ImageIcon("img/capricornio.jpg");
+						return "Capricornio";
+					} else {
+						icono = new ImageIcon("img/acuario.jpg");
+						return "Acuario";
+					}
+				case 2:
+					if(Integer.parseInt(fecha[0])<19) {
+						icono = new ImageIcon("img/acuario.jpg");
+						return "Acuario";
+					} else {
+						icono = new ImageIcon("img/piscis.jpg");
+						return "Piscis";
+					}
+				case 3:
+					if(Integer.parseInt(fecha[0])<21) {
+						icono = new ImageIcon("img/piscis.jpg");
+						return "Piscis";
+					} else {
+						icono = new ImageIcon("img/aries.jpg");
+						return "Aries";
+					}
+				case 4:
+					if(Integer.parseInt(fecha[0])<20) {
+						icono = new ImageIcon("img/aries.jpg");
+						return "Aries";
+					} else {
+						icono = new ImageIcon("img/tauros.jpg");
+						return "Tauro";
+					}
+				case 5:
+					if(Integer.parseInt(fecha[0])<21) {
+						icono = new ImageIcon("img/tauros.jpg");
+						return "Tauro";
+					} else {
+						icono = new ImageIcon("img/geminis.jpg");
+						return "Géminis";
+					}
+				case 6:
+					if(Integer.parseInt(fecha[0])<21) {
+						icono = new ImageIcon("img/geminis.jpg");
+						return "Géminis";
+					} else {
+						icono = new ImageIcon("img/cancer.jpg");
+						return "Cáncer";
+					}
+				case 7:
+					if(Integer.parseInt(fecha[0])<23) {
+						icono = new ImageIcon("img/cancer.jpg");
+						return "Cáncer";
+					} else {
+						icono = new ImageIcon("img/leo.jpg");
+						return "Leo";
+					}
+				case 8:
+					if(Integer.parseInt(fecha[0])<23) {
+						icono = new ImageIcon("img/leo.jpg");
+						return "Leo";
+					} else {
+						icono = new ImageIcon("img/virgo.jpg");
+						return "Virgo";
+					}
+				case 9:
+					if(Integer.parseInt(fecha[0])<23) {
+						icono = new ImageIcon("img/virgo.jpg");
+						return "Virgo";
+					} else {
+						icono = new ImageIcon("img/libra.jpg");
+						return "Libra";
+					}
+				case 10:
+					if(Integer.parseInt(fecha[0])<23) {
+						icono = new ImageIcon("img/libra.jpg");
+						return "Libra";
+					} else {
+						icono = new ImageIcon("img/escorpio.jpg");
+						return "Escorpio";
+					}
+				case 11:
+					if(Integer.parseInt(fecha[0])<22) {
+						icono = new ImageIcon("img/escorpio.jpg");
+						return "Escorpio";
+					} else {
+						icono = new ImageIcon("img/sagitario.jpg");
+						return "Sagitario";
+					}
+				case 12:
+					if(Integer.parseInt(fecha[0])<22) {
+						icono = new ImageIcon("img/sagitario.jpg");
+						return "Sagitario";
+					} else {
+						icono = new ImageIcon("img/capricornio.jpg");
+						return "Capricornio";
+					}
+					default:
+						return "Fecha inválida";
+			}
 		
 	}	
 	
