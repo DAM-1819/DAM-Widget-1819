f9ce91b0a40a8cf6733b67e8a4ab53be5a04bea3
Unknown <manuel_lol@hotmail.com>
2018-11-14 19:54:02 +0100

    Widget noticias version 1.0.

--- src/RamPrincipal/Principal.java
+++ src/RamPrincipal/Principal.java
@@ -1,10 +1,41 @@
 package RamPrincipal;
 
-public class Principal {
+
+
+import java.awt.EventQueue;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.jsoup.Connection.Response;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import Redondo_Garcia_Jesus.VentanaPrincipal;
+
+public class Principal{
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+
+		EventQueue.invokeLater(new Runnable() {
+		public void run() {
+			try {
+				VentanaPrincipal2 ventanaPrincipal = new VentanaPrincipal2();
+				ventanaPrincipal.inicializar();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	});	
 	}
-
 }
---
+++ src/RamPrincipal/VentanaPrincipal2.java
@@ -0,0 +1,92 @@
+package RamPrincipal;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Rectangle;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import org.jsoup.Jsoup;
+import org.jsoup.Connection.Response;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import com.sun.prism.shader.Solid_TextureYV12_AlphaTest_Loader;
+
+
+
+public class VentanaPrincipal2 {
+	//La ventana principal, en este caso, guarda todos los componentes:
+	JFrame ventana;
+	WidgetNoticias not;
+
+	//Constructor, marca el tamaño y el cierre del frame
+	public VentanaPrincipal2() {
+		ventana = new JFrame();
+		ventana.setBounds(0, 0, 300,600);
+		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+	
+	/**
+	 * Método que inicializa todos los componentes de la ventana
+	 */
+	public void inicializarComponentes(){
+		
+		//Definimos el layout:
+		ventana.setLayout(new GridBagLayout());
+	GridBagConstraints gb= new GridBagConstraints();
+	//gb.gridx=0;
+	//gb.gridy=0;
+	gb.fill=GridBagConstraints.BOTH;
+	gb.weightx=1;
+	gb.weighty=1;
+
+		not= new WidgetNoticias();
+	
+		ventana.add(not,gb);
+		
+	
+	}
+	
+
+
+	
+
+	/**
+	 * Método que inicializa todos los listeners del programa.
+	 */
+	public void inicializarListeners(){
+	
+		
+	}
+	
+
+	/**
+	 * Método que realiza todas las llamadas necesarias para inicializar la ventana correctamente.
+	 */
+	public void inicializar(){
+		ventana.setVisible(true);
+		inicializarComponentes();	
+		inicializarListeners();		
+	}
+
+
+}
