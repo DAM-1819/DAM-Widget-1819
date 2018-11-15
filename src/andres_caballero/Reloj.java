48c8b0a412bbfa0760b525f1a9ab6d5fa00dfd0b
acaballeroa06 <draphugamer@gmail.com>
2018-11-15 16:17:54 +0100

    Casi finalizado

--- src/andres_caballero/Reloj.java
+++ src/andres_caballero/Reloj.java
@@ -1,5 +1,6 @@
 package andres_caballero;
 
+import java.awt.Font;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -8,36 +9,40 @@ import javax.swing.JLabel;
 
 public class Reloj extends JLabel implements Runnable {
 
-	private String hora, min, seg;
+	private String hora, minutos, segundos;
 	private Calendar calendario = new GregorianCalendar();
 	Thread hilo;
 
 	public Reloj(int x, int y, int p, int p1) {
-		super();
 		setBounds(x, y, p, p1);
+		setFont(new Font("Serif",14, 48));
 		hilo = new Thread(this);
-		//hilo.start();
+		hilo.start();
 	}
 
-	public void actualizar() {
+	public void actualiza() {
 		Date fechaHoraActual = new Date();
 		calendario.setTime(fechaHoraActual);
 
-		hora = 
+		hora = String.valueOf(calendario.get(Calendar.HOUR_OF_DAY));
+		minutos = calendario.get(Calendar.MINUTE) > 9 ? "" + calendario.get(Calendar.MINUTE)
+				: "0" + calendario.get(Calendar.MINUTE);
+		segundos = calendario.get(Calendar.SECOND) > 9 ? "" + calendario.get(Calendar.SECOND)
+				: "0" + calendario.get(Calendar.SECOND);
 	}
 
 	@Override
 	public void run() {
-		Thread hiloActual = Thread.currentThread();
-		while (hiloActual == hilo) {
-			actualizar();
-		}
-		this.setText(hora + ":" + min + ":" + seg);
-		try {
-			Thread.sleep(10);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		Thread ct = Thread.currentThread();
+		while (ct == hilo) {
+			try {
+				actualiza();
+				setText("" + hora + ":" + minutos + ":" + segundos);
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 }
--- src/andres_caballero/VentanaPrincipal.java
+++ src/andres_caballero/VentanaPrincipal.java
@@ -1,21 +1,25 @@
 package andres_caballero;
 
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+
 public class VentanaPrincipal {
 	JFrame ventana;
 	JPanel panel;
-	JLabel timeLabel;
+	Reloj timeLabel;
 
 	public VentanaPrincipal() {
-		this.ventana = new JFrame("Ventana Principal");
+		this.ventana = new JFrame("Reloj Digital");
 		ventana.setBounds(250, 150, 250, 150);
 		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
@@ -26,20 +30,19 @@ public class VentanaPrincipal {
 	}
 
 	public void inicializarComponentes() {
-		GridBagLayout gridBagLayout = new GridBagLayout();
-		ventana.getContentPane().setLayout(gridBagLayout);
+		GridLayout gridLayout = new GridLayout();
+		ventana.getContentPane().setLayout(gridLayout);
 
-		GridBagConstraints settings;
-
-		timeLabel = new JLabel();
-		timeLabel.setBackground(Color.RED);
+		panel = new JPanel();
+		panel.setLayout(gridLayout);
+		panel.setBorder(BorderFactory.createLineBorder(Color.CYAN, 20));
+		
+		ventana.add(panel);
+		timeLabel = new Reloj(0, 0, 50, 0);
+		timeLabel.setHorizontalAlignment(JLabel.CENTER);
 		timeLabel.setOpaque(true);
-		settings = new GridBagConstraints();
-		settings.gridx = 0;
-		settings.gridy = 0;
-		settings.weightx = 1;
-		settings.weighty = 1;
-		settings.fill = GridBagConstraints.BOTH;
-		ventana.add(timeLabel, settings);
+		panel.add(timeLabel);
+
+		
 	}
 }
