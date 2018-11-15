32640bdf19f6f09be905f29d58955827e6711ba0
Jorge Ramos <jorgera1996@gmail.com>
2018-11-15 12:30:18 +0100

    Jbuttons, Jtextarea, cambio de color y eliminar nota añadido

--- src/Ramos_Gil_Jorge/MainWindow.java
+++ src/Ramos_Gil_Jorge/MainWindow.java
@@ -1,23 +1,33 @@
 package Ramos_Gil_Jorge;
 
+import java.awt.Color;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
+import javax.swing.JButton;
+import javax.swing.JColorChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class MainWindow {
 
 	private JFrame jframe;
-	private JLabel[][] jlabel = new JLabel[3][3];
+	private JButton[][] jbutton;
 	private int i;
 	private int j;
 	
 	public MainWindow() {
 		jframe = new JFrame("Principal");
-		jframe.setBounds(0, 0, 800, 600);
+		jframe.setBounds(0, 0, 600, 600);
+		jframe.setResizable(false);
 		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
@@ -25,13 +35,43 @@ public class MainWindow {
 		
 		GridBagConstraints settings;
 		jframe.setLayout(new GridBagLayout());
+		jbutton = new JButton[3][3];
 		for (i = 0; i < 3; i++) {
 			for (j = 0; j < 3; j++) {
-				jlabel[i][j] = new JLabel("Añade una nota");
+				jbutton[i][j] = new JButton("Añade una nota");
 				settings = new GridBagConstraints();
 				settings.gridx = i;
 				settings.gridy = j;
-				jlabel[i][j].addMouseListener(new MouseListener() {
+				settings.ipady = 165;
+				settings.ipadx = 75;
+				settings.fill = GridBagConstraints.BOTH;
+				jframe.add(jbutton[i][j], settings);
+			}
+		}
+	}
+	
+	public void inicializarListeners() {
+		
+		jbutton[0][0].addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				jbutton[0][0].setVisible(false);
+				jframe.remove(jbutton[0][0]);
+				jframe.revalidate();
+				jframe.repaint();
+				JTextArea jtextarea = new JTextArea();
+				jtextarea.setLineWrap(true);
+				jtextarea.setWrapStyleWord(true);
+				jtextarea.setBackground(Color.decode("#F0F8FF"));
+				GridBagConstraints settings;
+				settings = new GridBagConstraints();
+				settings.gridx = 0;
+				settings.gridy = 0;
+				settings.fill = GridBagConstraints.BOTH;
+				jframe.add(jtextarea, settings);
+				jtextarea.addMouseListener(new MouseListener() {
 					
 					@Override
 					public void mouseReleased(MouseEvent e) {
@@ -60,21 +100,29 @@ public class MainWindow {
 					@Override
 					public void mouseClicked(MouseEvent e) {
 						// TODO Auto-generated method stub
-						if (e.getButton() == MouseEvent.BUTTON1) {
-							jlabel[i][j].setVisible(false);
-							System.out.println(i);
-							System.out.println(j);
-							jframe.remove(jlabel[i][j]);
+						if (e.getButton() == MouseEvent.BUTTON3) {
+							String opciones[] = {"Cambiar color","Eliminar nota"};
+							int answer = JOptionPane.showOptionDialog(jframe, "¿Qué deseas hacer?", 
+														"Enhorabuena", JOptionPane.YES_NO_CANCEL_OPTION,JOptionPane.QUESTION_MESSAGE, null, opciones, opciones[0]);
+							if (answer == 0) {
+								Color color = JColorChooser.showDialog(null, "Elige el color de texto", null);
+								jtextarea.setForeground(color);
+							} else if (answer == 1) {
+								jtextarea.setVisible(false);
+								jframe.remove(jtextarea);
+								jframe.revalidate();
+								jframe.repaint();
+							}
 						}
 					}
 				});
-				jframe.add(jlabel[i][j], settings);
 			}
-		}
+		});
 	}
 	
 	public void inicializar() {
 		jframe.setVisible(true);
 		inicializarComponentes();
+		inicializarListeners();
 	}
 }
