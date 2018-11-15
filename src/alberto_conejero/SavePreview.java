5341553dd6250b29ce4b706fca918e2691f8fdaa
AlbertoConejeroHernandez <aconejeroh02@iesvjp.es>
2018-11-15 03:23:07 +0100

    Comprobar el tipo de dato que llega desde el panel inicial

--- src/alberto_conejero/SavePreview.java
+++ src/alberto_conejero/SavePreview.java
@@ -1,18 +1,24 @@
 package alberto_conejero;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.image.BufferedImage;
 
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+
+import com.sun.xml.internal.stream.buffer.MutableXMLStreamBuffer;
 
 public class SavePreview extends JDialog {
 	/**
@@ -21,20 +27,43 @@ public class SavePreview extends JDialog {
 	JPanel panel;
 	JButton guardar;
 	JTextField url;
-	
+
 	/**
 	 * Objetos para visualizar en la preview lo que veamos
 	 */
 	JTextArea previewTexto;
 	JLabel imagen;
-	
-	public SavePreview() {
+	String textoArchivo;
+	Component panelMuestra;
+	ImageIcon image;
+
+	public SavePreview(Component c) {
 		super();
+		this.setModal(true);
 		this.setBounds(100, 100, 400, 500);
 		this.setLayout(new GridBagLayout());
-		
+		panelMuestra = c;
 		inicializaComp();
+		compruebaEntrada();
+		// inicializarListener();
 
+	}
+
+//	public void inicializarListener() {
+//		guardar.addActionListener(e->{
+//			if (url == url) {
+//				
+//			}
+//		});
+//	}
+
+	public boolean compruebaEntrada() {
+		if (panelMuestra.toString().equalsIgnoreCase("Javax.swing.JtextArea")) {
+			textoArchivo = panelMuestra.toString();
+			return true;
+		}
+
+		return false;
 	}
 
 	public void inicializaComp() {
@@ -46,9 +75,9 @@ public class SavePreview extends JDialog {
 		panel.setBorder(BorderFactory.createTitledBorder("Preview"));
 		sett = new GridBagConstraints();
 		sett.gridx = 0;
-		sett.gridy =0;
+		sett.gridy = 0;
 		sett.ipadx = 300;
-		sett.ipady=300;
+		sett.ipady = 300;
 		sett.fill = GridBagConstraints.BOTH;
 		this.add(panel, sett);
 		/**
@@ -65,17 +94,29 @@ public class SavePreview extends JDialog {
 		 */
 		guardar = new JButton("GUARDAR");
 		sett = new GridBagConstraints();
-		sett.gridx= 0;
+		sett.gridx = 0;
 		sett.gridy = 2;
 		sett.fill = GridBagConstraints.BOTH;
 		this.add(guardar, sett);
-		
+
 		/**
-		 * Inicializo  el textArea y lo añado al panel
+		 * Inicializo el textArea y lo añado al panel
 		 */
-		previewTexto = new JTextArea("asdada");
-		previewTexto.setEditable(false);
-		panel.add(previewTexto);
+		if (compruebaEntrada()) {
+			previewTexto = new JTextArea();
+			previewTexto.setEditable(false);
+			previewTexto.setSize(300, 300);
+			previewTexto.setText(textoArchivo);
+			panel.add(previewTexto);
+		} else {
+			/**
+			 * Inicializo el Label de la imagen y lo añado al panel
+			 */
+			imagen = new JLabel();
+			panel.add(imagen);
+		}
+
+		
 	}
 
 }
--- src/alberto_conejero/VentanaPrincipal.java
+++ src/alberto_conejero/VentanaPrincipal.java
@@ -1,25 +1,29 @@
 package alberto_conejero;
 
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class VentanaPrincipal {
 
 	JFrame ventana;
 	JButton muestraPanel;
-
+	Component prueba;
 	public VentanaPrincipal() {
 		ventana = new JFrame();
-		ventana.setBounds(100, 100, 150, 150);
+		ventana.setBounds(100, 100, 600, 600);
 		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
@@ -31,16 +35,25 @@ public class VentanaPrincipal {
 
 	public void inicializarListeners() {
 		muestraPanel.addActionListener(e -> {
-			SavePreview panel = new SavePreview();
+			SavePreview panel = new SavePreview(prueba);
 			panel.setVisible(true);
 		});
 	}
 
 	public void inicializarComponentes() {
 		ventana.setLayout(new GridBagLayout());
-		muestraPanel = new JButton("Prueba");
-		ventana.add(muestraPanel);
-
+		muestraPanel = new JButton("Guardar");
+		GridBagConstraints sett;
+		sett = new GridBagConstraints();
+		sett.gridx = 10;
+		sett.gridy = 10;
+		ventana.add(muestraPanel,sett);
+		prueba = new JTextArea(10, 10);
+		prueba.createImage(300, 300);
+		sett = new GridBagConstraints();
+		sett.gridx = 0;
+		sett.gridy = 0;
+		ventana.add(prueba,sett);
 	}
 
 }
