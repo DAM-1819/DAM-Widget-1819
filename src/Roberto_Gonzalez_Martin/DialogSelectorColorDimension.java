ee6198e10915a9453cdcd418c6188c90ae47257f
Roberto González <rgonzalezm72@iesvjp.es>
2018-11-15 15:33:48 +0100

    Interfaz completada

--- src/Roberto_Gonzalez_Martin/DialogSelectorColorDimension.java
+++ src/Roberto_Gonzalez_Martin/DialogSelectorColorDimension.java
@@ -8,6 +8,7 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
 import javax.swing.JTextField;
@@ -17,102 +18,190 @@ import javax.swing.event.ChangeListener;
 public class DialogSelectorColorDimension extends JDialog {
 
 	private static final long SerialVersionUID = 1L;
-	private JPanel tamanoVentana;
-	private JPanel colores;
-	private JPanel colorLetra;
-	private JPanel colorFondo;
-	private JSlider rojoLetra;
-	private JSlider verdeLetra;
-	private JSlider azulLetra;
+	//private JFrame frame;
+	private JPanel dimensionVentana;
+	private JPanel seleccionarColor;
+	private JPanel seleccionarColorFondo;
+	private JPanel seleccionarColorLetra;
+	private JPanel botonAceptar;
+	private JTextField ancho;
+	private JTextField numeroAncho;
+	private JTextField alto;
+	private JTextField numeroAlto;
+	private JTextField fondo;
 	private JSlider rojoFondo;
 	private JSlider verdeFondo;
 	private JSlider azulFondo;
-	private JPanel color;
+	private JPanel colorFondo;
+    private JTextField letra;
+	private JSlider rojoLetra;
+	private JSlider verdeLetra;
+	private JSlider azulLetra;
+	private JPanel colorLetra;
 	private JButton aceptar;
 	private Color rgb;
 
 	public DialogSelectorColorDimension() {
+		//this.frame = frame;
 		setModal(true);
-		setBounds(0, 0, 400, 600);
+		setBounds(0, 0, 500, 500);
 		anadirElementos();
 		anadirListeners();
 		this.setVisible(true);
 	}
 
 	private void anadirElementos() {
-		this.setLayout(new GridLayout(2,1));
+		this.setLayout(new GridLayout(3,1));
+		
+		dimensionVentana = new JPanel();
+		dimensionVentana.setLayout(new GridLayout(2,2));
+		ancho = new JTextField("Ancho de la ventana: ");
+		ancho.setEditable(false);
+		numeroAncho = new JTextField();
+		alto = new JTextField("Alto de la ventana: ");
+		alto.setEditable(false);
+		numeroAlto = new JTextField();
+		dimensionVentana.add(ancho);
+		dimensionVentana.add(numeroAncho);
+		dimensionVentana.add(alto);
+		dimensionVentana.add(numeroAlto);
+		
+		seleccionarColor = new JPanel();
+		seleccionarColor.setLayout(new GridLayout(1,2));
+		
 		GridBagConstraints settingsObjeto = new GridBagConstraints();
-		tamanoVentana = new JPanel();
-		colores = new JPanel();
-		rojoLetra = new JSlider(0, 255);
-		verdeLetra = new JSlider(0, 255);
-		azulLetra = new JSlider(0, 255);
-		rojoFondo = new JSlider(0, 255);
-		verdeFondo = new JSlider(0, 255);
-		azulFondo = new JSlider(0, 255);
-		color = new JPanel();
-		aceptar = new JButton("Aceptar");
-
+		seleccionarColorFondo = new JPanel();
+		seleccionarColorFondo.setLayout(new GridBagLayout());
+		fondo = new JTextField("Color fondo:");
+		fondo.setEditable(false);
 		settingsObjeto.gridx = 0;
 		settingsObjeto.gridy = 0;
-		colorLetra.add(rojoLetra, settingsObjeto);
-
+		seleccionarColorFondo.add(fondo,settingsObjeto);
+		rojoFondo = new JSlider(0, 255);
 		settingsObjeto.gridx = 0;
 		settingsObjeto.gridy = 1;
-		colorLetra.add(verdeLetra, settingsObjeto);
-
+		seleccionarColorFondo.add(rojoFondo,settingsObjeto);
+		verdeFondo = new JSlider(0, 255);
 		settingsObjeto.gridx = 0;
 		settingsObjeto.gridy = 2;
-		colorLetra.add(azulLetra, settingsObjeto);
-
+		seleccionarColorFondo.add(verdeFondo,settingsObjeto);
+		azulFondo = new JSlider(0, 255);
 		settingsObjeto.gridx = 0;
-		settingsObjeto.gridy = 7;
+		settingsObjeto.gridy = 3;
+		seleccionarColorFondo.add(azulFondo,settingsObjeto);
+		colorFondo = new JPanel();
+		settingsObjeto.gridx = 0;
+		settingsObjeto.gridy = 4;
 		settingsObjeto.ipadx = 100;
 		settingsObjeto.ipady = 30;
 		settingsObjeto.fill = GridBagConstraints.BOTH;
-		colorLetra.add(color, settingsObjeto);
-
+		seleccionarColorFondo.add(colorFondo, settingsObjeto);
+		seleccionarColor.add(seleccionarColorFondo);
+		
+		settingsObjeto = new GridBagConstraints();
+		seleccionarColorLetra = new JPanel();
+		seleccionarColorLetra.setLayout(new GridBagLayout());
+		letra = new JTextField("Color letra:");
+		letra.setEditable(false);
+		settingsObjeto.gridx = 0;
+		settingsObjeto.gridy = 0;
+		seleccionarColorLetra.add(letra,settingsObjeto);
+		rojoLetra = new JSlider(0, 255);
+		settingsObjeto.gridx = 0;
+		settingsObjeto.gridy = 1;
+		seleccionarColorLetra.add(rojoLetra,settingsObjeto);
+		verdeLetra = new JSlider(0, 255);
+		settingsObjeto.gridx = 0;
+		settingsObjeto.gridy = 2;
+		seleccionarColorLetra.add(verdeLetra,settingsObjeto);
+		azulLetra = new JSlider(0, 255);
+		settingsObjeto.gridx = 0;
+		settingsObjeto.gridy = 3;
+		seleccionarColorLetra.add(azulLetra,settingsObjeto);
+		colorLetra = new JPanel();
+		settingsObjeto.gridx = 0;
+		settingsObjeto.gridy = 4;
+		settingsObjeto.ipadx = 100;
+		settingsObjeto.ipady = 30;
+		settingsObjeto.fill = GridBagConstraints.BOTH;
+		seleccionarColorLetra.add(colorLetra, settingsObjeto);
+		seleccionarColor.add(seleccionarColorLetra);
+		
+		botonAceptar = new JPanel();
+		botonAceptar.setLayout(new GridBagLayout());
+		aceptar = new JButton("Aceptar");
 		settingsObjeto.gridx = 0;
 		settingsObjeto.gridy = 8;
 		settingsObjeto.ipadx = 10;
 		settingsObjeto.ipady = 5;
 		settingsObjeto.fill = GridBagConstraints.BOTH;
-		this.add(aceptar, settingsObjeto);
+		botonAceptar.add(aceptar, settingsObjeto);
+		this.add(dimensionVentana);
+		this.add(seleccionarColor);
+		this.add(botonAceptar);
 	}
 
 	private void anadirListeners() {
 
+		rojoFondo.addChangeListener(new ChangeListener() {
+
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				rgb = new Color(rojoFondo.getValue(), verdeFondo.getValue(), azulFondo.getValue());
+				colorFondo.setBackground(rgb);
+			}
+		});
+
+		verdeFondo.addChangeListener(new ChangeListener() {
+
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				rgb = new Color(rojoFondo.getValue(), verdeFondo.getValue(), azulFondo.getValue());
+				colorFondo.setBackground(rgb);
+			}
+		});
+
+		azulFondo.addChangeListener(new ChangeListener() {
+
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				rgb = new Color(rojoFondo.getValue(), verdeFondo.getValue(), azulFondo.getValue());
+				colorFondo.setBackground(rgb);
+			}
+		});
+		
 		rojoLetra.addChangeListener(new ChangeListener() {
 
 			@Override
 			public void stateChanged(ChangeEvent e) {
 				rgb = new Color(rojoLetra.getValue(), verdeLetra.getValue(), azulLetra.getValue());
-				color.setBackground(rgb);
+				colorLetra.setBackground(rgb);
 			}
 		});
-
+		
 		verdeLetra.addChangeListener(new ChangeListener() {
 
 			@Override
 			public void stateChanged(ChangeEvent e) {
 				rgb = new Color(rojoLetra.getValue(), verdeLetra.getValue(), azulLetra.getValue());
-				color.setBackground(rgb);
+				colorLetra.setBackground(rgb);
 			}
 		});
-
+		
 		azulLetra.addChangeListener(new ChangeListener() {
 
 			@Override
 			public void stateChanged(ChangeEvent e) {
 				rgb = new Color(rojoLetra.getValue(), verdeLetra.getValue(), azulLetra.getValue());
-				color.setBackground(rgb);
+				colorLetra.setBackground(rgb);
 			}
 		});
 
 		aceptar.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
+				//frame.setBounds(100, 100, Integer.parseInt(numeroAncho.getText()), Integer.parseInt(numeroAlto.getText()));
+				//frame.setBackground(rgb);
 			}
 		});
 	}
--- src/Roberto_Gonzalez_Martin/VentanaPrincipal.java
+++ src/Roberto_Gonzalez_Martin/VentanaPrincipal.java
@@ -13,6 +13,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.border.TitledBorder;
@@ -20,56 +21,41 @@ import javax.swing.border.TitledBorder;
 public class VentanaPrincipal {
 
 	private JFrame frame;
-	private JTextField titulo;
-	private JTextField contenido;
 	private JButton widget;
+	private JTextField titulo;
+	private JTextField texto;
 
 	public VentanaPrincipal() {
 		frame = new JFrame("Widget Roberto González Martín");
-		frame.setBounds(100, 100, 500, 500);
+		frame.setBounds(100, 100, 700, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 	public void inicializarComponentes() {
 		frame.setLayout(new GridBagLayout());
 		GridBagConstraints settingsObjeto = new GridBagConstraints();
-		titulo = new JTextField("Página de ejemplo");
+		titulo = new JTextField("Editor de las dimensiones de ventana y el color de fondo y texto");
 		titulo.setEditable(false);
-		titulo.setHorizontalAlignment(SwingConstants.CENTER);
 		settingsObjeto.gridx = 0;
 		settingsObjeto.gridy = 0;
-		settingsObjeto.weightx = 4;
-		settingsObjeto.weighty = 1;
-		settingsObjeto.fill = GridBagConstraints.BOTH;
 		frame.add(titulo,settingsObjeto);
-		contenido = new JTextField(
-				"Lorem Ipsum is simply dummy text of the printing and typesetting industry./n"
-				+"/n Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-				+"/n It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. "
-				+"/n It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, "
-				+"/n and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
-		contenido.setEditable(false);
-		contenido.setHorizontalAlignment(SwingConstants.CENTER);
+		texto = new JTextField("Este widget está hecho para cambiar el tamaño de la ventana y el color de la letra y el fondo a su gusto");
+		texto.setEditable(false);
 		settingsObjeto.gridx = 0;
 		settingsObjeto.gridy = 1;
-		settingsObjeto.weightx = 4;
-		settingsObjeto.weighty = 10;
-		settingsObjeto.fill = GridBagConstraints.BOTH;
-		frame.add(contenido,settingsObjeto);
+		frame.add(texto,settingsObjeto);
 		widget = new JButton("Widget");
 		settingsObjeto.gridx = 0;
-		settingsObjeto.gridy = 3;
-		settingsObjeto.weightx = 1;
-		settingsObjeto.weighty = 1;
+		settingsObjeto.gridy = 2;
 		frame.add(widget,settingsObjeto);
 	}
 
 	public void inicializarListener() {
 		widget.addActionListener(new ActionListener() {
-
+			
 			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				DialogSelectorColorDimension selector = new DialogSelectorColorDimension();
+			public void actionPerformed(ActionEvent e) {
+				DialogSelectorColorDimension dialogo = new DialogSelectorColorDimension();
 			}
 		});
 	}
