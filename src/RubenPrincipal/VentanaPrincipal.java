137397e7550511380f02f011f955a4eb0cf2d7fd
Ruben <rhernandezz01@informatica.iesvalledeljerteplasencia.es>
2018-11-15 13:29:49 +0100

    Primera Versión
    Cosas a mejorar : redimensionar imágenes

--- src/RubenPrincipal/VentanaPrincipal.java
+++ src/RubenPrincipal/VentanaPrincipal.java
@@ -1,27 +1,40 @@
 package RubenPrincipal;
 
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class VentanaPrincipal {
 
 	JFrame ventana;
 	JPanel panelCorazon;
-	ImageIcon corazon;
-	JLabel imagenLabel;
+
+	Button quitarVida;
+	JLabel[] imagenLabel;
+	static int VIDAS = 3;
+	ImageIcon corazonG = new ImageIcon(("src/RubenPrincipal/corazonG.jpg"));
+	// Vidas vidas;
 
 	public VentanaPrincipal() {
 		ventana = new JFrame();
-		ventana.setLayout(new GridLayout(1, 3));
+		ventana.getContentPane().setLayout(new GridBagLayout());
 		ventana.setBounds(300, 300, 900, 500);
 		ventana.setDefaultCloseOperation(1);
-
+		ventana.getContentPane().setBackground(Color.WHITE);
 	}
 
 	public void iniciar() {
@@ -32,32 +45,75 @@ public class VentanaPrincipal {
 	}
 
 	private void inciarComponenetes() {
-		for (int i = 0; i < 3; i++) {
-			corazon = new ImageIcon("src/RubenPrincipal/corazon.jpg");
-			imagenLabel = new JLabel(corazon);
-					ventana.add(imagenLabel);
-
-		}
 
 		ventana.setVisible(true);
 		ventana.setSize(1000, 500);
-		ventana.setResizable(false);
+		ventana.setResizable(true);
+		int width = 60;
+		int height = 60;
+		quitarVida = new Button("Quitar vida");
+		imagenLabel = new JLabel[3];
+		ImageIcon corazon = new ImageIcon(("src/RubenPrincipal/corazon.jpg"));
+		for (int i = 0; i < imagenLabel.length; i++) {
+			GridBagConstraints gbc = new GridBagConstraints();
+			gbc.gridx = i;
+			gbc.gridy = 0;
+			gbc.weightx = 1;
+			gbc.weighty = 1;
+			imagenLabel[i] = new JLabel(corazon);
+			ventana.add(imagenLabel[i], gbc);
 
-		// for (int i = 0; i < 3; i++) {
-		// panelCorazon = new JPanel();
-		// //panelCorazon.add(corazon);
-		// }
-		// ImageIcon imagenCorazon = new ImageIcon(
-		// getClass().getResource("/DAM-Widget-1819/src/RubenPrincipal/corazon.jpg"));
-		// imagenLabel.add(panelCorazon);
-		// // ImageIO.read(new File("DAM-Widget-1819/src/RubenPrincipal/corazon.jpg"));
-		// //
-		// // devuelve imagen
-		// // redimensionar la imagen para meter en icono
+		}
+		// para poner el boton
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		gbc.gridwidth = 3;
+		gbc.fill = gbc.BOTH;
+		gbc.weightx = 3;
+		gbc.weighty = 1;
+		ventana.add(quitarVida, gbc);
 
 	}
 
 	private void inicarListeners() {
+
+		quitarVida.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+				restarVida(true);
+			}
+		});
+
+	}
+
+	private void restarVida(boolean restar) {
+		if (restar == true) {
+			if (VIDAS == 3) {
+				imagenLabel[0].setIcon(corazonG);
+
+				VIDAS--;
+				System.out.println(VIDAS);
+			} else {
+				if (VIDAS == 2) {
+					imagenLabel[1].setIcon(corazonG);
+					VIDAS--;
+					System.out.println(VIDAS);
+
+				} else {
+					if (VIDAS == 1) {
+						imagenLabel[2].setIcon(corazonG);
+						VIDAS--;
+						JOptionPane.showMessageDialog(null, "GAME OVER");
+						System.exit(0);
+					}
+				}
+			}
+		} else {
+			System.out.println("No restamos.");
+		}
 
 	}
 
